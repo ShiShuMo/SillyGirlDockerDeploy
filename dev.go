7a157db6d7e867e8e8e1c10232fad433bb077b9a
莫诗书 <36871700+ShiShuMo@users.noreply.github.com>
2022-02-15 15:33:52 +0800

    Update dev.go

--- dev.go
+++ dev.go
@@ -7,7 +7,7 @@ import (
 	_ "github.com/cdle/sillyGirl/im/wxmp"
 	_ "github.com/cdle/sillyGirl/im/wx"
 	//_ "github.com/cdle/sillyGirl/develop/jd_price"
-	_ "github.com/cdle/sillyGirl/develop/jd_cookie"
+	//_ "github.com/cdle/sillyGirl/develop/jd_cookie"
 	//_ "github.com/cdle/sillyGirl/develop/cable"
-	_ "github.com/cdle/sillyGirl/develop/onebyone"
+	//_ "github.com/cdle/sillyGirl/develop/onebyone"
 )
